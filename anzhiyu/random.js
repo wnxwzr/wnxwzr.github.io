6acd4fc7a7b823bede9fb9a9288a3951e1bfede5
wnxwzr <wnxwzr@gmail.com>
2024-02-23 18:21:14 +0800

    Site updated: 2024-02-23 18:21:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/23/hello-world/","2024/02/23/搞渗透/","2024/02/23/搞编程/","2024/02/23/搞逆向/"];function toRandomPost(){
+var posts=["2024/02/23/搞渗透/","2024/02/23/hello-world/","2024/02/23/搞编程/","2024/02/23/搞逆向/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
