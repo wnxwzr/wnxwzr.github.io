3714ad52c3d518defe9574a182ffad9817a1972e
wnxwzr <wnxwzr@gmail.com>
2024-02-25 23:11:25 +0800

    Site updated: 2024-02-25 23:11:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/25/渗透攻防/Android APP渗透之系统设备信息检测绕过/"];function toRandomPost(){
+var posts=["2024/02/25/渗透攻防/APP渗透之系统设备信息检测绕过/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
