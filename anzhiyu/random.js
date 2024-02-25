3cd44d01697c23531568826e0ab38f3c20d0917c
wnxwzr <wnxwzr@gmail.com>
2024-02-25 23:44:45 +0800

    Site updated: 2024-02-25 23:44:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/25/渗透攻防/APP渗透之系统设备信息检测绕过/"];function toRandomPost(){
+var posts=["2024/02/25/渗透攻防/APP渗透之系统设备信息检测绕过/","2024/02/25/渗透攻防/公众号渗透数据包解密/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
