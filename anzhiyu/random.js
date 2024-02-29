971261a5d12fd16d3166333ebe5e1214fddc0b41
wnxwzr <wnxwzr@gmail.com>
2024-03-01 00:26:14 +0800

    Site updated: 2024-03-01 00:26:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/26/杂项/Hexo文章页面插入B站视频/","2024/02/25/渗透攻防/APP渗透之系统设备信息检测绕过/","2024/02/26/渗透攻防/使用reqable联动burpsuite抓取APP数据包/","2024/02/25/渗透攻防/公众号渗透数据包解密/"];function toRandomPost(){
+var posts=["2024/02/29/Android/Android应用开发基础/","2024/02/26/杂项/Hexo文章页面插入B站视频/","2024/02/26/渗透攻防/使用reqable联动burpsuite抓取APP数据包/","2024/02/25/渗透攻防/公众号渗透数据包解密/","2024/02/25/渗透攻防/APP渗透之系统设备信息检测绕过/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
