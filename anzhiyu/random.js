7ac68069f393e212dd86022c77c09d8388afce97
wnxwzr <wnxwzr@gmail.com>
2024-03-05 15:07:30 +0800

    Site updated: 2024-03-05 15:07:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/29/Android/Android应用开发基础/","2024/03/05/Harmony/Harmony开发简介/","2024/03/05/Harmony/OpenHarmony北向开发环境搭建/","2024/02/26/杂项/Hexo文章页面插入B站视频/","2024/02/26/渗透攻防/使用reqable联动burpsuite抓取APP数据包/","2024/02/25/渗透攻防/APP渗透之系统设备信息检测绕过/","2024/02/25/渗透攻防/公众号渗透数据包解密/"];function toRandomPost(){
+var posts=["2024/02/29/Android/Android应用开发基础/","2024/03/05/Harmony/Harmony开发简介/","2024/03/05/Harmony/OpenHarmony北向开发环境搭建/","2024/02/26/杂项/Hexo文章页面插入B站视频/","2024/02/25/渗透攻防/APP渗透之系统设备信息检测绕过/","2024/02/26/渗透攻防/使用reqable联动burpsuite抓取APP数据包/","2024/02/25/渗透攻防/公众号渗透数据包解密/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
