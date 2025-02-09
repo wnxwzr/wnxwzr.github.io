6b1ad2caf527d783481b5584b9363d4187a8c83f
wnxwzr <wnxwzr@gmail.com>
2025-02-09 21:38:51 +0800

    Site updated: 2025-02-09 21:38:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/hello-world/"];function toRandomPost(){
+var posts=["2023/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
