1a53e0fe0f7b6d007ff2b8ef0d067053d237bdcd
wnxwzr <wnxwzr@gmail.com>
2025-02-12 00:27:57 +0800

    Site updated: 2025-02-12 00:27:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/hello-world/"];function toRandomPost(){
+var posts=["2025/3029211222.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
